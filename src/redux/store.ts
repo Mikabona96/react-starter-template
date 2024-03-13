f0d67e9c6c322fb11516ebdf621a37e526ec0a92
Dmytro Holub <dmitriygolubapk@gmail.com>
2024-03-13 21:44:07 +0300

    redux devTools disabled for production

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -15,6 +15,7 @@ const middleware: Middleware[] = [postsApi.middleware];
 export const store = configureStore({
   reducer: rootReducer,
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(middleware),
+  devTools: process.env.NODE_ENV !== 'production',
 });
 
 export type RootState = ReturnType<typeof store.getState>;
