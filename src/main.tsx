90fab2718e2f9de1f7d7785d36cff5f4a6c289dc
Dmytro Holub <dmitriygolubapk@gmail.com>
2024-03-13 16:58:34 +0300

    styled-components GlobalStyle added 💅🏼

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,13 @@ import { ThemeProvider } from 'styled-components';
 import { store } from './redux/store.ts';
 import { router } from './router.ts';
 import { theme } from './assets/theme/theme.ts';
+import { GlobalStyle } from './assets/theme';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <RouterProvider router={router} />
       </ThemeProvider>
     </Provider>
